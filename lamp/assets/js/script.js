a85a9618f09d8328a4967229279ca347096100f9
André Silva <andrenunessilva1996@gmail.com>
2023-01-21 15:16:25 -0300

    Light On and Off

---
+++ lamp/assets/js/script.js
@@ -0,0 +1,16 @@
+document.querySelector('.button').addEventListener('click', () => {
+    let button = document.querySelector('.button');
+    let lampImg = document.querySelector('#lamp-img');
+
+    if(button.value === 'Ligar') {
+        lampImg.setAttribute('src', 'assets/img/lamp-on.png');
+        lampImg.setAttribute('alt', 'Lâmpada ligada');
+        document.body.style.backgroundColor = '#000'
+        button.value = 'Desligar';
+    } else if (button.value === 'Desligar') {
+        lampImg.setAttribute('src', 'assets/img/lamp-off.png');
+        lampImg.setAttribute('alt', 'Lâmpada desligada');
+        document.body.style.backgroundColor = '#fff';
+        button.value = 'Ligar';
+    }
+});
