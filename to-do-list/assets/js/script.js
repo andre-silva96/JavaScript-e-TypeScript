b1b13de1e64c9f9fec5643b4db94616e384221d4
André Silva <andrenunessilva1996@gmail.com>
2023-01-21 15:14:19 -0300

    Basic To Do List

---
+++ to-do-list/assets/js/script.js
@@ -0,0 +1,23 @@
+document.querySelector('#input-button').addEventListener('click', addToDoList);
+
+function addToDoList() {
+    let input = document.querySelector('#input-text');
+    let sectionList = document.querySelector('.to-do-list-section');
+    let ul = document.querySelector('ul');
+
+    if(input.value != '') {
+        sectionList.appendChild(ul);
+
+        createItem(input, ul);
+
+        input.value = '';
+    } else {
+        alert('Preencha o campo!');
+    }
+}
+
+function createItem(input, ul) {
+    let li = document.createElement('li');
+    li.innerText += input.value;
+    ul.appendChild(li);
+}
